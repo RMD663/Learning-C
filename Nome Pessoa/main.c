797642c729231285be77b2f3e55904feaa3b57d1
Ryan M <142442993+RMD663@users.noreply.github.com>
2024-02-03 13:28:13 -0300

    Add files via upload

---
+++ Nome Pessoa/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+char Nome[15];
+printf("Insira seu nome: ");
+scanf("%s", Nome);
++
+int Idade;
+printf("Insira sua idade: ");
+scanf("%d", &Idade);
+
+printf("Seu nome E %s E sua idade E %d", Nome, Idade);
+
+
+
+return 0;
+}
